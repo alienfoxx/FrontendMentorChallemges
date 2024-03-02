ff337a6bb0818566f3856b5298092cad47a70549
alienfoxx <alienfoxx@gmail.com>
2024-03-02 17:47:18 +0100

    FAQ accordion challenge

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const questions = document.querySelectorAll(".question");
+
+    questions.forEach(question => {
+        question.addEventListener("click", function() {
+            const answer = this.querySelector('.answer');
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
